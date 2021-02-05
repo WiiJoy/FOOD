64dc74cf77ab554e293a129c85a9eb51d98fab16
Vitaliy <wiification@gmail.com>
2021-02-05 19:40:04 +0300

    Pack some items in function and delete comment code v.2

--- js/script.js
+++ js/script.js
@@ -99,7 +99,6 @@ window.addEventListener('DOMContentLoaded', () => {
     function openModal() {
         modal.classList.add('show');
         modal.classList.remove('hide');
-        // modal.classList.toggle('show');
         document.body.style.overflow = 'hidden';
         clearInterval(modalTimerId);
     }
@@ -111,7 +110,6 @@ window.addEventListener('DOMContentLoaded', () => {
     function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show');
-        // modal.classList.toggle('show');
         document.body.style.overflow = '';
     }
   
@@ -190,45 +188,12 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    // getResource('http://localhost:3000/menu')
-    //     .then(data => {
-    //         data.forEach(({img, altimg, title, descr, price}) => {
-    //             new FoodItem(img, altimg, title, descr, price, '.menu .container').render();
-    //         });
-    //     });
-
-        axios.get('http://localhost:3000/menu')
+    axios.get('http://localhost:3000/menu')
         .then(data => {
             data.data.forEach(({img, altimg, title, descr, price}) => {
                 new FoodItem(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
-    // new FoodItem(
-    //     "img/tabs/vegy.jpg",
-    //     "vegy",
-    //     'Меню "Фитнес"',
-    //     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    //     5,
-    //     '.menu .container',
-    // ).render();
-
-    // new FoodItem(
-    //     "img/tabs/elite.jpg",
-    //     "elite",
-    //     'Меню “Премиум”',
-    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    //     7,
-    //     '.menu .container',
-    // ).render();
-
-    // new FoodItem(
-    //     "img/tabs/post.jpg",
-    //     "post",
-    //     'Меню "Постное"',
-    //     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    //     6,
-    //     '.menu .container',
-    // ).render();
 
     // Forms
 
@@ -266,33 +231,12 @@ window.addEventListener('DOMContentLoaded', () => {
                 display: block;
                 margin: 0 auto;
             `;
-            // form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
 
-            // const request = new XMLHttpRequest();
-            // request.open('POST', 'server.php');
-
-            // request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
-
-            // const object = {};
-            // formData.forEach(function(value, key) {
-            //     object[key] = value;
-            // });
 
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            // const json = JSON.stringify(object);
-
-            // request.send(json);
-
-            // fetch('server.php', {
-            //     method: "POST",
-            //     headers: {
-            //         'Content-type': 'application/json'
-            //     },
-            //     body: json = JSON.stringify(object)
-            // })
             postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
@@ -304,17 +248,6 @@ window.addEventListener('DOMContentLoaded', () => {
             }).finally(() => {
                 form.reset();
             });
-
-            // request.addEventListener('load', () => {
-            //     if (request.status === 200) {
-            //         console.log(request.response);
-            //         showThanksModal(message.success);
-            //         form.reset();
-            //         statusMessage.remove();
-            //     } else {
-            //         showThanksModal(message.failure);
-            //     }
-            // });
         });
     }
 
